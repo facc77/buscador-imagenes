f8335f495511572d03175dc28763b5717bd7937d
facc77 <ferrerfacundo96@gmail.com>
2021-06-13 20:08:18 -0300

    commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Error from "./Error";
+import PropTypes from "prop-types";
+
+const Formulario = ({ guardarBusqueda }) => {
+  const [termino, guardarTermino] = useState("");
+  const [error, guardarError] = useState(false);
+
+  const buscarImagenes = (e) => {
+    e.preventDefault();
+    //validar
+    if (termino.trim() === "") {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+    //enviar el termino de busqueda a App
+    guardarBusqueda(termino);
+  };
+
+  return (
+    <form onSubmit={buscarImagenes}>
+      <div className="row">
+        <div className="form-group col-md-8">
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            onChange={(e) => guardarTermino(e.target.value)}
+            placeholder="Busca una imagen, ejemplo: futbol o cafe"
+          />
+        </div>
+        <div className="form-group col-md-4">
+          <input
+            type="submit"
+            className="btn btn-lg btn-danger btn-block"
+            value="Buscar"
+          />
+        </div>
+      </div>
+
+      {error ? <Error mensaje="Agrega un término de busqueda" /> : null}
+    </form>
+  );
+};
+
+Formulario.propTypes = {
+  guardarBusqueda: PropTypes.func.isRequired,
+};
+
+export default Formulario;
